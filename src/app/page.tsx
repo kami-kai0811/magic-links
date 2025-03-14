039f6239495ca4dafd3532dfcfe11aaa84d7e6de
kaito <kamiya0811eng@gmail.com>
2025-03-14 22:23:55 +0900

    sign in ページの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SingIn from '@/components/sign-in';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex justify-center items-center container mx-auto bg-background">
       <SingIn />
     </div>
   );
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,23 +1,33 @@
 import { signIn } from '@/configuration/auth';
+import { Button, buttonVariants } from './ui/button';
+import { cn } from '@/lib/utils';
 
 export default function SingIn() {
   return (
-    <form
-      action={async (formData: FormData) => {
-        'use server';
-        await signIn('resend', formData);
-      }}
-      className="flex flex-col items-center gap-5"
-    >
-      <input
-        name="email"
-        placeholder="...@example.com"
-        type="text"
-        className="border-2 w-2xs"
-      />
-      <button type="submit" className="cursor-pointer border bg-amber-300">
-        Sign in with Resend
-      </button>
-    </form>
+    <div className="flex flex-col gap-8 text-center">
+      <h2 className="text-5xl">sign In</h2>
+      <form
+        action={async (formData: FormData) => {
+          'use server';
+          await signIn('resend', formData);
+        }}
+        className="flex flex-col items-center gap-5"
+      >
+        <input
+          name="email"
+          placeholder="user@example.com"
+          type="text"
+          className="border-2 w-2xs"
+        />
+        <Button
+          className={cn(
+            buttonVariants({ variant: 'default' }),
+            'cursor-pointer'
+          )}
+        >
+          sign In
+        </Button>
+      </form>
+    </div>
   );
 }
